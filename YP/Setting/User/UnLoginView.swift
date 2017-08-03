7eff8d7c2d7a36c8f7c722fa227e9d5b02a9777c
peter <DywanePeter@gmail.com>
2017-08-03 18:53:26 +0800

    添加设置页面

--- YP/Setting/User/UnLoginView.swift
+++ YP/Setting/User/UnLoginView.swift
@@ -10,5 +10,30 @@ import UIKit
 
 class UnLoginView: UIView {
 
+    @IBOutlet weak var loginButton: UIButton!
+    @IBOutlet weak var loginLabel: UILabel!
+    @IBOutlet weak var tableView: UITableView!
 
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        loginButton.layer.cornerRadius = 5
+        loginButton.layer.masksToBounds = true
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+}
+
+extension UnLoginView: UITableViewDataSource, UITableViewDelegate{
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+    }
 }
